83739777203114ad501ae758ef80af06cb5e1685
barshp <bars@sela.co.il>
2019-09-25 17:55:45 +0300

    Removed redundant code

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -34,7 +34,6 @@ export class AppComponent implements OnInit, OnDestroy {
     this.msManager.getMicroserviceNames(path).subscribe(
       (ms) => {
         this.store.dispatch(new MicroservicesActions.Set({ names: ms, path: path, pathIsValid: !!ms.length }));
-        console.log(this.microservices);
       },
       (err) => {
         this.store.dispatch(new MicroservicesActions.Set({ names: [], path: path, pathIsValid: false }));
@@ -43,6 +42,6 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   runMicroservices(msNames: string[]): void {
-    this.msManager.startMicroservices(this.microservices.path, msNames).subscribe(console.log);
+    this.msManager.startMicroservices(this.microservices.path, msNames).subscribe();
   }
 }
